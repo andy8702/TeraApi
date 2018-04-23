1b36e8edc1a3c56cbb017a8bed55a9d5a8f06601
AndyZhou <yang.zhou@ncr.com>
2018-04-23 15:32:09 +0800

    commit 4

--- TerawellnessApi/app/src/main/java/com/zy/tera/Constants.java
+++ TerawellnessApi/app/src/main/java/com/zy/tera/Constants.java
@@ -3,8 +3,4 @@ package com.zy.tera;
 public class Constants {
     public static String BASEURL= "http://139.224.133.23/";
     public static String BOOK_BASEURL= "http://180.169.65.108:8081/";
-
-    public static String DEV1= "http://180.169.65.108:8081/";
-    public static String DEV2= "http://180.169.65.108:8081/";
-    public static String DEV3= "http://180.169.65.108:8081/";
 }
