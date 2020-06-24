9c5095db21e60f2e2a5755770a08c21b1cc27809
seababy_8702@163.com <seababy_8702@163.com>
2020-06-24 17:10:55 +0800

    添加数据库支持

--- TerawellnessApi/app/src/main/java/com/zy/tera/RateSettingActivity.kt
+++ TerawellnessApi/app/src/main/java/com/zy/tera/RateSettingActivity.kt
@@ -2,13 +2,18 @@ package com.zy.tera
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.zy.tera.db.CoachRateInfo
+import com.zy.tera.db.TeraDatabase
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_ratesetting_rate.*
 
 class RateSettingActivity: BaseActivity() {
 
     private lateinit  var coachRateInfo:CoachRateInfo;
-
+    private val disposable = CompositeDisposable();
 
     companion object{
        const val FLAG_isRating:String = "isRate";
@@ -38,8 +43,24 @@ class RateSettingActivity: BaseActivity() {
 
         ratingbar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
 
+            coachRateInfo.rate = rating;
             Log.d("rating",rating.toString());
 
+            disposable.add(TeraDatabase.getInstance(this).coachRateInfoDao().insertUser(coachRateInfo)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        Toast.makeText(this,R.string.oper_success,Toast.LENGTH_SHORT).show();
+                        finish();
+                    },
+                            { error -> Log.e("db", "Unable to insert", error) }))
+
+
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        disposable.clear();
     }
 }
--- TerawellnessApi/app/src/main/java/com/zy/tera/db/CoachRateInfo.kt
+++ TerawellnessApi/app/src/main/java/com/zy/tera/db/CoachRateInfo.kt
@@ -2,9 +2,22 @@ package com.zy.tera.db
 
 import android.annotation.SuppressLint
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
+
+
 @SuppressLint("ParcelCreator")// 用于处理 Lint 的错误提示
 @Parcelize
-data class CoachRateInfo(val id:String,val name:String):Parcelable {
-    var rate:Int? = null;
+@Entity(tableName = "coachrateinfo")
+data class CoachRateInfo(
+        @PrimaryKey
+        @ColumnInfo(name = "id")
+        val id:String,
+        @ColumnInfo(name = "name")
+        val name:String):Parcelable {
+
+    @ColumnInfo(name = "rate")
+    var rate:Float? = null;
 }
