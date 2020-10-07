d2e062d99733d5e2a3f84b752dabfafe861ba92f
Yang ZHOU <zhouyang@sheca.com>
2020-10-07 22:20:57 +0800

    kt新首页

---
+++ TerawellnessApi/app/src/main/kotlin/com/zy/kt/tera/pages/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.zy.kt.tera.pages;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Yang ZHOU on 2019/6/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ TerawellnessApi/app/src/main/kotlin/com/zy/kt/tera/pages/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zy.kt.tera.pages
+
+import android.os.Bundle
+import com.zy.tera.R
+
+class MainActivity:BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.kt_activity_main);
+    }
+}
