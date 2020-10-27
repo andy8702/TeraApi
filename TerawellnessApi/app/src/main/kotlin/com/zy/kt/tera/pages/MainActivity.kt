357e9d76bfcec9f6fc2b7c260d5c0cdf1fba943f
Yang ZHOU <zhouyang@sheca.com>
2020-10-27 23:02:10 +0800

    增加首页跳转FLUTTER页面

--- TerawellnessApi/app/src/main/kotlin/com/zy/kt/tera/pages/MainActivity.kt
+++ TerawellnessApi/app/src/main/kotlin/com/zy/kt/tera/pages/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.zy.kt.tera.pages
 
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.zy.tera.R
+import io.flutter.embedding.android.FlutterActivity
+import kotlinx.android.synthetic.main.kt_activity_main.*
 
 class MainActivity:BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.kt_activity_main);
+
+        Glide.with(this).load(R.drawable.terawellnesslogo).into(image);
+
+        image.postDelayed(Runnable {
+            startActivity(FlutterActivity.createDefaultIntent(this))
+            finish()
+        },3000)
+
     }
 }
